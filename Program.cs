31be4c9e3392e8dcd6934ab597663a406e040393
Juan Manuel Valiño <jmvdm09@gmail.com>
2018-09-03 21:42:49 -0300

    Repo de prueba

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Expert_SRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Critique el siguiente código en base a SRP y patrón Experto
+            //Realice la modificaciones necesaras para que se cumplan ambos patrones
+            
+            Alfajor a = new Alfajor(10, 8.5);
+            Kiosko k = new Kiosko();
+            if (k.PuedeComprar(a, 15, "$"))
+            {
+                Console.WriteLine("Felicitaciones! Tiene suficiente dinero para comprar un alfajor :)");
+            }
+            else
+            {
+                Console.WriteLine("Mejor consiga un trabajo :(");
+            }
+        }
+    }
+}
